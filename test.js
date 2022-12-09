77ad614ddbf63f5e814f10513c7431b91e8b85a5
S0Iomon <73535112+S0Iomon@users.noreply.github.com>
2022-12-09 08:15:26 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,22 +1,49 @@
-
 let button = document.querySelector('#click')
+let output = document.querySelector('.output')
 let obj = {}
 
-let func = () => {
+let answers = () => {
     let string = document.querySelector('.answer').value
     let text = string.split('').sort()
-
-    let count = 2;
-
-    for (let i = 0; i < text.length; i++){
-        text[i] == text[i+1] ? obj[text[i]] = count++: count = 2
-    }
-
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+    let count = 0;
+    alphabet.forEach(el => {
+        for (let i = 0; i < text.length; i++){
+            if (el.match(text[i])){
+                count++
+                obj[el] = count;
+            } else {
+                count = 0;
+            }
+        }
+    })
     console.log(obj)
+
+    let keys = Object.keys(obj)
+    let values = Object.values(obj)
+    let index = 0;
+    let arr = [];
+    
+    let recursion = () => {
+        while (index < keys.length){
+            arr.push(`${keys[index]}:${values[index]}`)
+            index++
+            if (index == keys.length){
+                break
+            }
+        }
+    }
+    recursion()
+    output.value = arr.join("\r\n")
+    output.style.fontWeight = 'bold'
 }
 
-button.addEventListener('click', func)
-console.log(obj)
+
+button.addEventListener('click', answers)
+button.addEventListener('click', e => {
+    e.preventDefault()
+})
+
 
 
 
